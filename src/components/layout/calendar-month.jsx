94a27d061afbe5febee0b978c35f97bbf8b5ff2a
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-12-19 09:29:00 -0800

    Added padding to the calendar header, months now changed based on what you press

--- src/components/layout/calendar-month.jsx
+++ src/components/layout/calendar-month.jsx
@@ -11,9 +11,8 @@ import { useState, useEffect } from 'react'
 
 
 
-//moment().datyInMonth gives you days in the month
+
 const Month = () => {
-  const months = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
   const [currentMonth, setCurrentMonth] = useState(moment().month())
   const [ currentYear, setCurrentYear ] = useState(moment().year())
 
@@ -52,38 +51,36 @@ const Month = () => {
     }
   }, [currentYear])
 
-
   return (
     <div className={calendarStyles.header}>
-    <div className={calendarStyles.year}>
-      <Heading variant='h4'>{currentYear}</Heading>
-    </div>
-    <div className={calendarStyles.month}>
-      <div>
-        <button name='last-month' className={calendarStyles.button} onClick={lastYear}>
-          <BiChevronsLeft />
-        </button>
-        <button id='last-year' name='last-year' className={calendarStyles.button} onClick={lastMonth}>
-          <BiChevronLeft />
-        </button>
+      <div className={calendarStyles.year}>
+        <Heading variant='h4'>{currentYear}</Heading>
       </div>
-      <div className={calendarStyles.monthsList}>
-        {months.map(month => <Heading key={`${month}`} variant='h4'>
-          {moment()
-            .month(month)
-            .format('MMM')}
+      <div className={calendarStyles.month}>
+        <div>
+          <button name='last-month' className={calendarStyles.button} onClick={lastYear}>
+            <BiChevronsLeft />
+          </button>
+          <button id='last-year' name='last-year' className={calendarStyles.button} onClick={lastMonth}>
+            <BiChevronLeft />
+          </button>
+        </div>
+        <div className={calendarStyles.monthsList}>
+         <Heading variant='h4'>
+            { moment()
+               .month(currentMonth)
+               .format('MMM')}
           </Heading>
-          )}
+        </div>
+        <div>
+          <button name='next-month' className={calendarStyles.button} onClick={nextMonth}>
+            <BiChevronRight />
+          </button>
+          <button name='next-year' className={calendarStyles.button} onClick={nextYear}>
+            <BiChevronsRight />
+          </button>
+        </div>
       </div>
-      <div>
-        <button name='next-month' className={calendarStyles.button} onClick={nextMonth}>
-          <BiChevronRight />
-        </button>
-        <button name='next-year' className={calendarStyles.button} onClick={nextYear}>
-          <BiChevronsRight />
-        </button>
-      </div>
-    </div>
     </div>
   )
 }
